38c07752a1368dca0a8f41009052959abeb8eb90
qwer9052 <qwer9052@gmail.com>
2020-02-12 17:25:48 +0900

    Add UserController.java[login&updateq]

--- UserController.java
+++ UserController.java
@@ -17,6 +17,14 @@ public class UserController{
 		return "user/insert";
 	}
  
+	//회원수정 페이지로 이동
+	@RequestMapping("/user/update.do")
+	public String login(){
+		logger.info("경로 : update"):
+		
+		return "user/update";
+	}
+ 
 
 
 }
