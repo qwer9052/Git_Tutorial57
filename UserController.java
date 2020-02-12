fdbbc17f2b57b84ffe6326e75ce25a55064ec73f
qwer9052 <qwer9052@gmail.com>
2020-02-12 17:59:58 +0900

    Add UserController.java [Delete]

--- UserController.java
+++ UserController.java
@@ -24,6 +24,15 @@ public class UserController{
 		
 		return "user/update";
 	}
+
+	//회원삭제 페이지로 이동
+	@RequestMapping("/user/delete.do")
+	public String login(){
+		logger.info("경로 : delete"):
+		
+		return "user/delete";
+	}
+
  
 
 
