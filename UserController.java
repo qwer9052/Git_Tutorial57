b25962e04f42e996a5ce1b44edfc6b4d1f761faf
qwer9052 <qwer9052@gmail.com>
2020-02-12 17:16:11 +0900

    Add UserController[Loginn]

--- UserController.java
+++ UserController.java
@@ -8,4 +8,15 @@ public class UserController{
 		
 		return "user/login";
 	}
+
+	//회원가입 페이지로 이동
+	@RequestMapping("/user/insert.do")
+	public String login(){
+		logger.info("경로 : insert"):
+		
+		return "user/insert";
+	}
+ 
+
+
 }
